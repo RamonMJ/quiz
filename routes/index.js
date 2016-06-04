f5ce9d4ce4406f412138286c78386aacab99f819
RamonMJ <ramonmartinezjimenez@gmail.com>
2016-06-05 01:46:28 +0200

    Tema 25: roles y permisos

--- routes/index.js
+++ routes/index.js
@@ -30,24 +30,24 @@ router.delete('/session', sessionController.destroy); // destruir sesión
 router.get('/quizzes.:format?', quizController.index);
 router.get('/quizzes/:quizId(\\d+).:format?', quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
-router.get('/quizzes/new', quizController.new);
-router.post('/quizzes', quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizzes/:quizId(\\d+)', quizController.update);
-router.delete('/quizzes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizzes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizzes', sessionController.loginRequired, quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',sessionController.loginRequired, quizController.ownershipRequired, quizController.edit);
+router.put('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.ownershipRequired, quizController.update);
+router.delete('/quizzes/:quizId(\\d+)', sessionController.loginRequired, quizController.ownershipRequired, quizController.destroy);
 
 //Definición de rutas de comentarios
 router.get('/quizzes/:quizId(\\d+)/comments/new', sessionController.loginRequired, commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments', sessionController.loginRequired, commentController.create);
-router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', sessionController.loginRequired, commentController.accept);
+router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', sessionController.loginRequired, quizController.ownershipRequired, commentController.accept);
 
 // Definicion de rutas de cuenta
 router.get('/users',                    userController.index);   // listado usuarios
 router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
 router.get('/users/new',                userController.new);     // formulario sign un
 router.post('/users',                   userController.create);  // registrar usuario
-router.get('/users/:userId(\\d+)/edit', userController.edit);     // editar información de cuenta
-router.put('/users/:userId(\\d+)',      userController.update);   // actualizar información de cuenta
-router.delete('/users/:userId(\\d+)',   userController.destroy);  // borrar cuenta
+router.get('/users/:userId(\\d+)/edit', userController.ownershipRequired, userController.edit);     // editar información de cuenta
+router.put('/users/:userId(\\d+)',      userController.ownershipRequired, userController.update);   // actualizar información de cuenta
+router.delete('/users/:userId(\\d+)',   userController.ownershipRequired, userController.destroy);  // borrar cuenta
 
 module.exports = router;
